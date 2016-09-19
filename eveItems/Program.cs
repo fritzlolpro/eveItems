f4f8b76e9542d7b58db4dce7b26a101998b4acef
fritzlolpro <f.sorokin@mail.msk>
2016-09-19 19:08:09 +0300

    async requests

--- eveItems/Program.cs
+++ eveItems/Program.cs
@@ -27,6 +27,8 @@ namespace eveItems
 
         async static Task Process()
         {
+            int counter = 10;
+
             using (var input = new StreamReader(path))
             {
                 var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention(), ignoreUnmatched: true);
@@ -35,13 +37,32 @@ namespace eveItems
 
                 foreach (var elem in types)
                 {
-                    int typeId = elem.Key;
-                    string name = elem.Value.name.en;
-                    Console.WriteLine("{0} - {1}", typeId, name);
+
+                    if (counter < 0)
+                    {
+                        //break;
+                        await Task.Delay(1000);
+                        counter = 10;
+                    }
+                        int typeId = elem.Key;
+                        string name = elem.Value.name.en;
+
+                        var data = GetMarketData(typeId).Result;
+                        Console.WriteLine("{0},{1},{2},{3},{4},{5}", typeId, name, data.sell.fivePercent, data.sell.volume, data.buy.fivePercent, data.buy.volume);
+                    
+                    counter--;
                 }
+
             }
 
-            var data = GetMarketData(34).Result;
+            /*
+            Console.WriteLine("sel: {0}, volume: {1}", data.sell.fivePercent, data.sell.volume);
+            Console.WriteLine("buy: {0}, volume: {1}", data.buy.fivePercent, data.buy.volume);
+            */
+        }
+
+        private static void writeCsvFile(string data)
+        {
 
         }
 
@@ -50,19 +71,19 @@ namespace eveItems
             using (var httpClient = new HttpClient())
             {
                 var stringData = await httpClient.GetStringAsync(EveCentralURL + typeId);
-                return JsonConvert.DeserializeObject<List<EveCentral.MarketType> >(stringData)[0];
+                return JsonConvert.DeserializeObject<List<EveCentral.MarketType>>(stringData)[0];
             }
         }
 
-        
-        async private static Task<T> GetJsonFromWebLimited<T>( string url, Func<Task<T>> action )
+
+        async private static Task<T> GetJsonFromWebLimited<T>(string url, Func<Task<T>> action)
         {
-            if(DateTime.Now - lastTimeFrame > TimeSpan.FromSeconds(1))
+            if (DateTime.Now - lastTimeFrame > TimeSpan.FromSeconds(1))
             {
                 requestCount = 0;
                 lastTimeFrame = DateTime.Now;
             }
-            if(requestCount > requestsPerSecond)
+            if (requestCount > requestsPerSecond)
             {
                 await Task.Delay(1000);
             }
